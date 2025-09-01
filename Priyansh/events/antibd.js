f94f7f89b6ed6eee18bf8d1c6b6f648daf4e81da
anuragmishra22071999-sudo <anuragmishra22071999@gmail.com>
2025-09-01 20:26:42 +0530

    Update antibd.js

--- Priyansh/events/antibd.js
+++ Priyansh/events/antibd.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - 𝙏𝙐𝙈 𝘽𝙊𝙏 𝙆𝘼 𝙉𝙄𝘾𝙆𝙉𝘼𝙈𝙀 𝘾𝙃𝘼𝙉𝙂𝙀 𝙉𝙄 𝙆𝘼𝙍 𝙎𝘼𝙆𝙏𝘼𝙔 😹🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - तुम बोट का नाम नहीं बदल सकते हो समझे बाबू ये सिर्फ अनुराग मिश्रा का है`}, threadID);
     }  
         }
